0652a7aa06f8e93d855753f8aa471be92c052e75
VanoKi <kirsvano@gmail.com>
2025-05-19 19:58:50 +0300

    feat: deleteTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {TodolistItem} from "./components/TodolistItem.tsx";
+import {useState} from "react";
 
 export type Task = {
   id: number
@@ -8,31 +9,21 @@ export type Task = {
 }
 
 function App() {
-  const tasks1: Task[] = [
+  const [tasks, setTasks] = useState([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
-  ]
-  const tasks2: Task[] = [
-    /*{ id: 1, title: 'Hello world', isDone: true },
-    { id: 2, title: 'I am Happy', isDone: false },
-    { id: 3, title: 'Yo', isDone: false },*/
-  ]
+  ])
 
   const deleteTask = (taskID: number) => {
-    alert(taskID)
+    setTasks(tasks.filter(task => task.id !== taskID))
   }
 
   return (
       <div className="app">
         <TodolistItem
           title={'What to learn'}
-          tasks={tasks1}
-          deleteTask={deleteTask}
-        />
-        <TodolistItem
-          title={'What to do'}
-          tasks={tasks2}
+          tasks={tasks}
           deleteTask={deleteTask}
         />
       </div>
