89577aef34f8a81e1d939e59f92fd7a09ba42566
VanoKi <kirsvano@gmail.com>
2025-06-23 16:06:10 +0300

    feat: add todolists

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,54 @@
 import './App.css'
 
 function App() {
+  const createId = () => crypto.randomUUID()
+  const todolists = [
+    {
+      id: createId(),
+      title: 'What to learn',
+      tasks: [
+        {id: '1', title: 'HTML&CSS', isDone: true},
+        {id: '2', title: 'JS', isDone: true},
+        {id: '3', title: 'React', isDone: false},
+      ]
+    },
+    {
+      id: createId(),
+      title: 'What to buy',
+      tasks: [
+        {id: '1', title: 'Milk', isDone: true},
+        {id: '2', title: 'Bread', isDone: false},
+      ]
+    }
+  ]
+  console.log(`${todolists[0].id}--${todolists[1].id}`)
+
   return (
-      <div className="app">
+    <div className="app">
+      <div>
+        <h3>What to learn</h3>
         <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
+          <input/>
+          <button>+</button>
+        </div>
+        <ul>
+          <li>
+            <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
+          </li>
+          <li>
+            <input type="checkbox" checked={true}/> <span>JS</span>
+          </li>
+          <li>
+            <input type="checkbox" checked={false}/> <span>React</span>
+          </li>
+        </ul>
+        <div>
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
         </div>
       </div>
+    </div>
   )
 }
 
