666c8f9bb5fb2a94b28effb417731743f576228b
VanoKi <kirsvano@gmail.com>
2025-06-24 15:44:25 +0300

    feat: create v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import './App.css'
+import {useState} from "react";
+
+export type Task = {
+  id: string,
+  title: string,
+  isDone: boolean
+}
+export type FilterType = 'all' | 'active' | 'completed'
 
 function App() {
+  const v1 = () => crypto.randomUUID()
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: v1(), title: 'HTML&CSS', isDone: true },
+    { id: v1(), title: 'JS', isDone: true },
+    { id: v1(), title: 'ReactJS', isDone: false },
+  ])
+
   return (
       <div className="app">
         <div>
