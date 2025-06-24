c8849cfcfd118c5e989f2de6ee48f5aadca2197c
VanoKi <kirsvano@gmail.com>
2025-06-24 18:32:30 +0300

    feat: onKeyDown

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import {Button} from "./Button.tsx";
 import {useState} from "react";
+import {KeyboardEvent} from "react";
 
 type Props = {
   addItem: (item:string) => void
@@ -19,12 +20,19 @@ export const Input = (props: Props) => {
       setError(true)
     }
   }
+  const onKeyDown = (e:KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onClickHandler()
+    }
+  }
 
   return (
     <div>
       <input
         onChange={(e) => setValue(e.currentTarget.value)}
-        value={value}/>
+        onKeyDown={onKeyDown}
+        value={value}
+      />
       <Button onClick={onClickHandler} title={'+'}/>
       {error && <div>The input can't be an empty</div>}
     </div>
