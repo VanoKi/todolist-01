a96673f373ee48ec9834210322ae8146a7b83797
VanoKi <kirsvano@gmail.com>
2025-06-27 18:56:01 +0300

    feat: todolist-reducer.test

--- src/components/TodolistItem.tsx
+++ src/components/TodolistItem.tsx
@@ -19,8 +19,19 @@ export const TodolistItem = (props: Props) => {
 
   const initialState: Todolist[] = []
   export const todolistReducer = (state: Todolist[] = initialState, action):Todolist[] => {
-
+    switch (action.type) {
+      case 'delete_todolist' : {
+        return state
+      }
+      default: return state
+    }
   }
+
+  type Actions = {
+    type: string
+    payload: any
+  }
+
   const mappedLlist = (tasks:Task[]) => {
     return tasks.map( task => {
         const changeTaskTitleHandler = (title:string) => {
