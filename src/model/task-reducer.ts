c79fa92ec2f056b2c956b0ed46af8f1c1c677e9b
VanoKi <kirsvano@gmail.com>
2025-07-16 17:27:01 +0300

    feat: add case create_todolist

--- src/model/task-reducer.ts
+++ src/model/task-reducer.ts
@@ -4,12 +4,13 @@ const initialState: TasksState = {}
 
 export const tasksReducer = (state: TasksState = initialState, action: Actions): TasksState => {
   switch (action.type) {
-    case '': {
-      return state
+    case 'create_todolist': {
+      return {...state, [action.payload.id] : []}
     }
     default:
       return state
   }
 }
 
-type Actions = any
+type Actions = CreateTodolistAction
+
