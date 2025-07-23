3a6b47624b67929d7de5608070c75f851e95b06c
VanoKi <kirsvano@gmail.com>
2025-07-23 15:44:35 +0300

    feat: dispatchToTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
-import {useState} from "react";
+import {useReducer, useState} from "react";
 import {Todolist} from "./components/Todolist.tsx";
+import {taskReducer} from "./model/task-reducer.ts";
 
 export type Task = {
   id: string,
@@ -12,22 +13,27 @@ export type FilterType = 'all' | 'active' | 'completed'
 function App() {
   const v1 = () => crypto.randomUUID()
   const [filter, setFilter] = useState('all')
-  const [tasks, setTasks] = useState<Task[]>([
+  const initial = [
     { id: v1(), title: 'HTML&CSS', isDone: true },
     { id: v1(), title: 'JS', isDone: true },
     { id: v1(), title: 'ReactJS', isDone: false },
-  ])
+  ]
+  const [tasks, dispatchToTasks] = useReducer(taskReducer, initial)
+
   const removeTask = (taskId:string) => {
-    setTasks(tasks.filter( t => t.id !== taskId))
+    // setTasks(tasks.filter( t => t.id !== taskId))
+    dispatchToTasks({type: 'REMOVE_TASK', id: taskId})
   }
   const addTask = (task:string) => {
-    const newTask = {id: v1(), title: task, isDone: false}
-    setTasks([newTask, ...tasks])
+    // const newTask = {id: v1(), title: task, isDone: false}
+    // setTasks([newTask, ...tasks])
+    dispatchToTasks({type: 'ADD_TASK', title: task})
   }
   const changeStatus = (taskId:string) => {
-    setTasks(tasks.map(task => task.id === taskId
-      ? {...task, isDone: !task.isDone}
-      : task))
+    // setTasks(tasks.map(task => task.id === taskId
+    //   ? {...task, isDone: !task.isDone}
+    //   : task))
+    dispatchToTasks({type: 'CHANGE_TASK', id: taskId})
   }
   const changeFiler = (filter:FilterType) => {
     switch (filter) {
