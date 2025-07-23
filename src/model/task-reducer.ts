16d54b504c2d5890e3f400c2ca9c933c6bfe43a3
VanoKi <kirsvano@gmail.com>
2025-07-23 15:07:18 +0300

    feat: add task-reducer

--- src/model/task-reducer.ts
+++ src/model/task-reducer.ts
@@ -1,8 +1,24 @@
 import {Task} from "../App.tsx";
 
 const initialState:Task[] = []
-// const Action =
+const Action = 'REMOVE_TASK' | 'ADD_TASK' | 'CHANGE_TASK' | 'CHANGE_FILTER'
 
 export const taskReducer = (state = initialState, actioin) => {
-
+  switch (actioin.type) {
+    case 'REMOVE_TASK': {
+      return state.filter((task) => task.id !== actioin.id)
+    }
+    case 'ADD_TASK': {
+      const newTask = {id: crypto.randomUUID(), title: actioin.title, isDone: false}
+      return {newTask, ...state}
+    }
+    case 'CHANGE_TASK': {
+      return state.map(task => task.id === actioin.id ? {...task, isDone: !task.isDone} : task)
+    }
+    // case 'CHANGE_FILTER': {
+    //   return
+    // }
+    default:
+      return state
+  }
 }
