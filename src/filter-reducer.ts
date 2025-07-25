cce44bacf022c7a7d2d367639e46712aebbba6ea
VanoKi <kirsvano@gmail.com>
2025-07-25 17:07:34 +0300

    feat: add filter-reducer

---
+++ src/filter-reducer.ts
@@ -0,0 +1,12 @@
+import {FilterType} from "./App.tsx";
+
+const initialFilter:FilterType = 'all'
+type Actions = {type: 'SET_FILTER', filter: FilterType}
+
+export const filterReducer = (state:FilterType = initialFilter, actions:Actions):FilterType => {
+  switch (actions.type) {
+    case 'SET_FILTER':
+      return actions.filter
+    default: return state
+  }
+}
