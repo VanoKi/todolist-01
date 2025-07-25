50459eb55d74abc03e8ab382b4631ce7209a2d8a
VanoKi <kirsvano@gmail.com>
2025-07-25 16:59:57 +0300

    feat: move logic to todolistItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import './App.css'
 import {Todolist} from "./components/Todolist.tsx";
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootState} from "./store.ts";
-import {useState} from "react";
 
 export type Task = {
   id: string,
@@ -12,38 +9,10 @@ export type Task = {
 export type FilterType = 'all' | 'active' | 'completed'
 
 function App() {
-  const tasks = useSelector((state: AppRootState) => state.tasks)
-  const dispatch = useDispatch();
-
-  // const v1 = () => crypto.randomUUID()
-  const [filter, setFilter] = useState<FilterType>('all')
-
-  const removeTask = (taskId:string) => {
-    dispatch({type: 'REMOVE_TASK', id: taskId})
-  }
-  const addTask = (task:string) => {
-    dispatch({type: 'ADD_TASK', title: task})
-  }
-  const changeStatus = (taskId:string) => {
-    dispatch({type: 'CHANGE_TASK_STATUS', id: taskId})
-  }
-  const changeFiler = (filter:FilterType) => {
-    switch (filter) {
-      case "active": return tasks.filter(task => !task.isDone )
-      case "completed": return tasks.filter(task => task.isDone )
-      default: return tasks
-    }
-  }
 
   return (
       <div className="app">
-        <Todolist
-          tasks={changeFiler(filter)}
-          removeTask={removeTask}
-          changeStatus={changeStatus}
-          setFilter={setFilter}
-          addTask={addTask}
-        />
+        <Todolist/>
       </div>
   )
 }
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,17 +1,33 @@
 // @flow
-import {FilterType, Task} from "../App.tsx";
 import {Button} from "./Button.tsx";
 import {Input} from "./Input.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootState} from "../store.ts";
+import {FilterType} from "../App.tsx";
+import {useState} from "react";
 
-type Props = {
-  tasks: Task[]
-  removeTask: (taskId:string) => void
-  changeStatus: (taskId:string) => void
-  setFilter: (filter:FilterType) => void
-  addTask: (task:string) => void
-};
-export const Todolist = (props: Props) => {
-  const {tasks, removeTask, changeStatus, setFilter, addTask} = props
+export const Todolist = () => {
+  const tasks = useSelector((state: AppRootState) => state.tasks);
+  const dispatch = useDispatch();
+  const removeTask = (taskId:string) => {
+    dispatch({type: 'REMOVE_TASK', id: taskId})
+  }
+  const addTask = (task:string) => {
+    dispatch({type: 'ADD_TASK', title: task})
+  }
+  const changeStatus = (taskId:string) => {
+    dispatch({type: 'CHANGE_TASK_STATUS', id: taskId})
+  }
+
+  const [filter, setFilter] = useState<FilterType>('all')
+  const changeFiler = (filter:FilterType) => {
+    switch (filter) {
+      case "active": return tasks.filter(task => !task.isDone )
+      case "completed": return tasks.filter(task => task.isDone )
+      default: return tasks
+    }
+  }
+
   return (
       <div>
         <h3>What to learn</h3>
@@ -19,7 +35,7 @@ export const Todolist = (props: Props) => {
           <Input addItem={addTask}/>
         </div>
         <ul>
-          {tasks.map(task => (
+          {changeFiler(filter).map(task => (
             <li key={task.id}>
               <input
                 type="checkbox"
